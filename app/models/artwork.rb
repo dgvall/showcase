bdac7c04ea278f42c8ced47a7aa67c8fe22a0299
Diego Valles <dgvall134@gmail.com>
2023-03-31 15:53:55 -0600

    Will fix macros for models. User.artworks should return artworks created by that user. User.liked_artworks should return artworks liked by that user. Artwork.user should return creator. Artwork.liked_by should return all users that liked an artwork.

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,7 +1,9 @@
 class Artwork < ApplicationRecord
   belongs_to :user
+
   has_many :user_likes
-  has_many :users, through: :user_likes
+  has_many :liked_by, through: :user_likes, source: :user
+
   has_many :artwork_tags
   has_many :tags, through: :artwork_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :artworks
+  
   has_many :user_likes
-  # has_many :artworks, through: :user_likes
+  has_many :liked_artworks, through: :user_likes, source: :artwork
 end
