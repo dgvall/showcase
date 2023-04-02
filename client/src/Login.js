23603b01bab8ed1baadd2740774be7a292d2d49c
Diego Valles <dgvall134@gmail.com>
2023-04-02 13:28:13 -0600

    Will add login functionality, and correct backend error

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const userData = {
+      username,
+      password
+    }
+
+    fetch('/login', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
 
   return (
     <div>
-      
-    </div>
+    <h2 className = "form-header">Login</h2>
+    <form className = "form" onSubmit = {handleSubmit}>
+      <input
+        className = "form-input"
+        onChange = {(e) => setUsername(e.target.value)}
+        value = {username}
+        placeholder = "Username"
+      />
+
+      <input
+        className = "form-input"
+        onChange = {(e) => setPassword(e.target.value)}
+        value = {password}
+        placeholder = "Password"
+      />
+
+      <button className = "form-button">Create Account</button>
+    </form>
+  </div>
   )
 }
 
