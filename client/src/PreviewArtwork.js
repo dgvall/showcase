3e7634c9dae7a9be2eefd88582d899569a56fc94
Diego Valles <dgvall134@gmail.com>
2023-04-05 02:48:34 -0600

    Will send user to artwork page or user profile depending on click of artwork title or username

--- client/src/PreviewArtwork.js
+++ client/src/PreviewArtwork.js
@@ -1,21 +1,30 @@
 import React, {useState} from 'react'
+import {useHistory} from 'react-router-dom'
 import './PreviewArtwork.css'
 
 function PreviewArtwork({id, image_url, tags, likes, title, user}) {
   const [hovered, setHovered] = useState(false)
+  const history = useHistory()
   return (
     <div
       className = "home-art-container"
       onMouseEnter = {() => setHovered(true)}
       onMouseLeave = {() => setHovered(false)}
     >
-      <img alt = {title} src = {image_url}/>
+      <img
+        alt = {title}
+        src = {image_url}
+      />
       {
         hovered &&
           <div className = "hover-details">
             <div>
-              <h3>{title}</h3>
-              <p>{user.username}</p> 
+              <h3
+                onClick = {() => history.push(`/users/${user.username}/artworks/${id}`)}
+              >{title}</h3>
+              <p
+                onClick = {() => history.push(`/users/${user.username}`)}
+              >{user.username}</p> 
             </div>
             <p>⭐ {likes}</p>
           </div>
